4aac88bd97658075a7bba88075855285569119c0
jbaile36 <jbailey26505@gmail.com>
2026-04-05 14:02:45 -0400

    Module 3 Assignment
    Module 3 Assignment complete

---
+++ Module_3/chohan_JASB.py
@@ -0,0 +1,102 @@
+# Joshua Bailey 4/5/2026 Module 3 Assignment 3.2
+# This is a brownfield development, editing the existing ChoHan
+# game to add a bonus for rolling a 2 (NI) or 7 (SHICHI).
+# and increase the house cut to 12% of the pot, 
+# as well as changed the input prompts to "JASB" for my initials.
+
+
+"""Cho-Han, by Al Sweigart al@inventwithpython.com
+The traditional Japanese dice game of even-odd.
+View this code athttps://nostarch.com/big-book-small-python-projects
+Tags: short, beginner, game"""
+
+import random, sys
+
+# Added 7 (shichi) to the dictionary for the bonus.
+JAPANESE_NUMBERS = {1: 'ICHI', 2: 'NI', 3: 'SAN',
+                    4: 'SHI', 5: 'GO', 6: 'ROKU', 7: 'SHICHI'}
+
+print('''Cho-Han, by Al Sweigart al@inventwithpython.com
+
+In this traditional Japanese dice game, two dice are rolled in a bamboo
+cup by the dealer sitting on the floor. The player must guess if the
+dice total to an even (cho) or odd (han) number.   
+*BONUS* If the dice total equals 2 (ni) or 7 (shichi), a bonus of
+ten mon will be added to the player's purse.
+''')
+
+purse = 5000
+while True:  # Main game loop.
+    # Place your bet:
+    print('You have', purse, 'mon. How much do you bet? (or QUIT)')
+    while True:
+        # Change input to initials, JASB
+        pot = input('JASB: ')
+        if pot.upper() == 'QUIT':
+            print('Thanks for playing!')
+            sys.exit()
+        elif not pot.isdecimal():
+            print('Please enter a number.')
+        elif int(pot) > purse:
+            print('You do not have enough to make that bet.')
+        else:
+            # This is a valid bet.
+            pot = int(pot)  # Convert pot to an integer.
+            break  # Exit the loop once a valid bet is placed.
+
+    # Roll the dice.
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+
+    print('The dealer swirls the cup and you hear the rattle of dice.')
+    print('The dealer slams the cup on the floor, still covering the')
+    print('dice and asks for your bet.')
+    print()
+    print('    CHO (even) or HAN (odd)?')
+
+    # Let the player bet cho or han:
+    while True:
+        # Change input to initials, JASB
+        bet = input('JASB: ').upper()
+        if bet != 'CHO' and bet != 'HAN':
+            print('Please enter either "CHO" or "HAN".')
+            continue
+        else:
+            break
+
+    # Reveal the dice results:
+    print('The dealer lifts the cup to reveal:')
+    print('  ', JAPANESE_NUMBERS[dice1], '-', JAPANESE_NUMBERS[dice2])
+    print('    ', dice1, '-', dice2)
+
+    # Determine if the player won:
+    rollIsEven = (dice1 + dice2) % 2 == 0
+    if rollIsEven:
+        correctBet = 'CHO'
+    else:
+        correctBet = 'HAN'
+
+    playerWon = bet == correctBet
+
+    # Bonus for totals 2 (NI) or 7 (SHICHI): add 10 mon to player's purse.
+    total = dice1 + dice2
+    if total in (2, 7):
+        print('Bonus! The dealer awards you 10 mon.')
+        purse += 10
+
+    # Display the bet results:
+    if playerWon:
+        print('You won! You take', pot, 'mon.')
+        purse = purse + pot  # Add the pot from player's purse.
+        # Changed house cut to 12%
+        print('The house collects a', pot // 12, 'mon fee.')
+        purse = purse - (pot // 12)  # The house fee is 12%.
+    else:
+        purse = purse - pot  # Subtract the pot from player's purse.
+        print('You lost!')
+
+    # Check if the player has run out of money:
+    if purse == 0:
+        print('You have run out of money!')
+        print('Thanks for playing!')
+        sys.exit()
